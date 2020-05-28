156311d5d6ace217988e538a11513ceee7017c8b
Ryley Lamb <ryleylamb@gmail.com>
2020-05-28 15:30:39 -0700

    Added Header Links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar';
 import logo from './portfolioPhoto.jpg';
 import './App.css';
+import NavbarBrand from 'react-bootstrap/NavbarBrand'
+import NavbarToggle from 'react-bootstrap/NavbarToggle'
+import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
+import Nav from 'react-bootstrap/Nav'
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Ryley Lamb",
+      headerLinks: [
+        {title: 'Home', path: '/'}, 
+        {title: 'About', path: '/about'} , 
+        {title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'Grand Admiral',
+        subTitle: 'Thrawn',
+        text: 'Welcome'
+      },
+      about: {
+        title: 'About me'
+      },
+      contact: {
+        title: "Contact Information"
+      }
+
+    }
+  }
+
+
+  render() 
+  {
+    return (
+
+      <Router>
+        <Container className="p-0" fluid={true}>
+
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <NavbarBrand>Ryley Lamb</NavbarBrand>
+              
+            <NavbarToggle className="border-0" aria-controls="navbar-toggle"/>
+            <NavbarCollapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/"> Home </Link>
+                <Link className="nav-link" to="/about"> About </Link>
+                <Link className="nav-link" to="/contact"> Contact </Link>
+              </Nav>
+            </NavbarCollapse>
+          </Navbar>
+
+
+        </Container>
+      </Router>
+
+      /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -12,8 +69,9 @@ function App() {
         </p>
         
       </header>
-    </div>
-  );
+    </div>*/
+    );
+  } 
 }
 
 export default App;
